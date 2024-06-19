d9a490eaaaa07f8fd5a917e868ae01a94ea890b8
Romina Rodriguez Tejedor <rominarodrigueztejedor@MBP-de-Romina.station>
2024-06-19 19:51:26 +0200

    Header,Footer,Categories,Home ,Index-actualizado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Typography, Container, Grid, Avatar, Box } from "@mui/material";
 import CategoryHome from "../../components/CategoryHome";
