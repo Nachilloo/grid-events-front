1969b8d72cba19510d972d30eb984f4e5cb274ac
Diego <dDac17@github.com>
2024-06-19 09:07:56 +0100

    not found update

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,21 +1,71 @@
+import { useEffect, useState } from 'react';
 
 const Profile = () => {
- 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token'); // Obtener el token de autenticación desde el almacenamiento local
+
+    fetch('/api/user', {
+      headers: {
+        'Authorization': `Bearer ${token}` // Incluir el token en el encabezado de la solicitud
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.text().then(text => {
+            throw new Error(text);
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error.message);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div>
       <h1>Mi Perfil</h1>
       <div>
-        <strong>Nombre:</strong> 
+        <strong>Nombre:</strong> {user.name}
       </div>
       <div>
-        <strong>Email:</strong> 
+        <strong>Email:</strong> {user.email}
       </div>
-     
       <div>
-        <strong>Eventos Registrados</strong>
+        <strong>Edad:</strong> {user.age}
+      </div>
+      <div>
+        <strong>Biografía:</strong> {user.bio}
+      </div>
+      <div>
+        <strong>Eventos Registrados:</strong>
+        <ul>
+          {user.events.map((event, index) => (
+            <li key={index}>{event}</li>
+          ))}
+        </ul>
       </div>
     </div>
- )    
+  );
 }
 
-export default Profile
+export default Profile;
+
