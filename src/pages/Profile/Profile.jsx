81d68eb9ec95c9a475b4dd4a66fd33b4936cca51
Romina <romi_soplido24@hotmail.com>
2024-06-25 21:42:18 +0200

    cambio Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -52,51 +52,69 @@ function Profile() {
 
   return (
     <Container>
-    <Typography variant="h4" gutterBottom>
-      Perfil de {userProfile.firstname}
-    </Typography>
-    <Card style={{ marginBottom: '20px' }}>
-      <CardContent style={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar
-          alt={userProfile.firstname}
-          src={userProfile.imgProfile}
-          style={{ width: 100, height: 100, marginRight: 20 }}
-        />
-        <div>
-          <Typography variant="h6">Información del Usuario</Typography>
-          <Typography variant="body1"><strong>Nombre:</strong> {userProfile.firstname}</Typography>
-          <Typography variant="body1"><strong>Apellido:</strong> {userProfile.lastname}</Typography>
-          <Typography variant="body1"><strong>Email:</strong> {userProfile.email}</Typography>
-          <Typography variant="body1"><strong>Género:</strong> {userProfile.gender}</Typography>
-          <Typography variant="body1"><strong>Ciudad:</strong> {userProfile.city}</Typography>
-          <Typography variant="body1"><strong>País:</strong> {userProfile.country}</Typography>
-          <Typography variant="body1"><strong>Teléfono:</strong> {userProfile.phone}</Typography>
-        </div>
-      </CardContent>
-    </Card>
-    <Container
-      maxWidth="lg"
-      style={{ paddingTop: "2rem", paddingBottom: "2rem" }}
-    >
-      <Typography variant="h6">Mis Eventos Favoritos</Typography>
-      {userProfile.events && userProfile.events.map(event => (
-          <EventCard key={event.id}> {/* Assign a unique key to each EventCard */}
-          <CardMedia
-            component="img"
-            height="350"
-            image={event.imgProfile}
-            alt={event.title}
+      <Typography variant="h4" gutterBottom>
+        Perfil de {userProfile.firstname}
+      </Typography>
+      <Card style={{ marginBottom: "20px" }}>
+        <CardContent style={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            alt={userProfile.firstname}
+            src={userProfile.imgProfile}
+            style={{ width: 100, height: 100, marginRight: 20 }}
           />
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {event.title}
+          <div>
+            <Typography variant="h6">Información del Usuario</Typography>
+            <Typography variant="body1">
+              <strong>Nombre:</strong> {userProfile.firstname}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {event.date}
+            <Typography variant="body1">
+              <strong>Apellido:</strong> {userProfile.lastname}
             </Typography>
-          </CardContent>
-        </EventCard>
-        ))}
+            <Typography variant="body1">
+              <strong>Email:</strong> {userProfile.email}
+            </Typography>
+            <Typography variant="body1">
+              <strong>Género:</strong> {userProfile.gender}
+            </Typography>
+            <Typography variant="body1">
+              <strong>Ciudad:</strong> {userProfile.city}
+            </Typography>
+            <Typography variant="body1">
+              <strong>País:</strong> {userProfile.country}
+            </Typography>
+            <Typography variant="body1">
+              <strong>Teléfono:</strong> {userProfile.phone}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+      <Container
+        maxWidth="lg"
+        style={{ paddingTop: "2rem", paddingBottom: "2rem" }}
+      >
+        <Typography variant="h6">Mis Eventos Favoritos</Typography>
+        {userProfile.events &&
+          userProfile.events.map((event) => (
+            <EventCard key={event.id}>
+              {" "}
+              {/* Assign a unique key to each EventCard */}
+              <CardMedia
+                component="img"
+                height="350"
+                image={event.imgProfile}
+                alt={event.title}
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {event.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {event.date}
+                </Typography>
+              </CardContent>
+            </EventCard>
+          ))}
+      </Container>
     </Container>
   );
 }
