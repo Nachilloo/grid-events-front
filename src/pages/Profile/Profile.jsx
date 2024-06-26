693b6c02b965eaa41998f3630bdd8ff414e95782
Romina <romi_soplido24@hotmail.com>
2024-06-26 21:51:49 +0200

    about

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -13,10 +13,12 @@ import {
   CardMedia,
 } from "@mui/material";
 import { styled } from "@mui/system";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const [userProfile, setUserProfile] = useState(null);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getUserDataProfile = async () => {
@@ -54,7 +56,10 @@ function Profile() {
       </Container>
     );
   }
-
+  const handleEditProfile = () => {
+    // Aquí podrías resetear formData con los datos originales del usuario si los tienes almacenados
+    navigate("/editprofile");
+  };
   const EventCard = styled(Card)(({ theme }) => ({
     minWidth: 300,
     margin: theme.spacing(2),
@@ -63,7 +68,6 @@ function Profile() {
 
   return (
     <Container>
-
       <Typography variant="h4" gutterBottom align="center" marginTop={"50px"}>
         Perfil de {userProfile.firstname} {userProfile.lastname}
       </Typography>
@@ -81,64 +85,6 @@ function Profile() {
             </Typography>
             <Typography variant="body1">
               <strong>Apellido:</strong> {userProfile.lastname}
-
-    <Typography variant="h4" gutterBottom>
-      Perfil de {userProfile.firstname}
-    </Typography>
-    <Card style={{ marginBottom: '30px' }}>
-      <CardContent style={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar
-          alt={userProfile.firstname}
-          src={userProfile.imgProfile}
-          style={{ width: 100, height: 100, marginRight: 20 }}
-        />
-        <div>
-          <Typography variant="h6">Información del Usuario</Typography>
-          <Typography variant="body1"><strong>Nombre:</strong> {userProfile.firstname}</Typography>
-          <Typography variant="body1"><strong>Apellido:</strong> {userProfile.lastname}</Typography>
-          <Typography variant="body1"><strong>Email:</strong> {userProfile.email}</Typography>
-          <Typography variant="body1"><strong>Género:</strong> {userProfile.gender}</Typography>
-          <Typography variant="body1"><strong>Ciudad:</strong> {userProfile.city}</Typography>
-          <Typography variant="body1"><strong>País:</strong> {userProfile.country}</Typography>
-          <Typography variant="body1"><strong>Teléfono:</strong> {userProfile.phone}</Typography>
-        </div>
-      </CardContent>
-    </Card>
-    <Container maxWidth="sm" sx={{ pt: 8, pb: 8 }}>
-      <Typography variant="h6">Mis Eventos Favoritos</Typography>
-      {userProfile.events && userProfile.events.map(event => (
-          <EventCard key={event.id}> {/* Assign a unique key to each EventCard */}
-          <CardMedia
-      component="img"
-      alt={event.title}
-      sx={{
-        height: 'auto', 
-        width: '100%',
-        objectFit: 'cover',
-      }}
-      image={event.imgProfile || "default-image-url.jpg"}
-      title={event.title}
-    />
-          <CardContent
-          sx={{
-            height: '130px', 
-            overflow: 'hidden',
-        }}>
-            <Typography variant="h5" component="div">
-              {event.title}
-            </Typography>
-            <Typography 
-            variant="body2"
-            color="text.secondary"
-            sx={{
-                display: '-webkit-box',
-                WebkitBoxOrient: 'vertical',
-                WebkitLineClamp: 3,
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-            }}>
-              {event.description}
-
             </Typography>
             <Typography variant="body1">
               <strong>Email:</strong> {userProfile.email}
@@ -156,7 +102,11 @@ function Profile() {
               <strong>Teléfono:</strong> {userProfile.phone}
             </Typography>
           </div>
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleEditProfile}
+          >
             Editar Perfil
           </Button>
         </CardContent>
